618e0157b6154632940162f881df02fb0f9ecfc9
Gregory Mitrano <BlairMLH@Hotmail.com>
2013-02-23 18:16:39 -0500

    *Added keyboard input to player GM

--- src/com/awesome/entity/Player.java
+++ src/com/awesome/entity/Player.java
@@ -2,6 +2,7 @@ package com.awesome.entity;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 import com.awesome.map.Map;
@@ -18,5 +19,28 @@ public class Player extends Entity {
 		boundingBox.setBounds(
 				boundingBox.getX(), boundingBox.getY(),
 				Map.TILE_SIZE, Map.TILE_SIZE);
+	}
+	
+	public void update(GameContainer gc, int delta) throws SlickException 
+	{
+		Input input = gc.getInput();
+		if (input.isKeyPressed(Input.KEY_W))
+		{
+			SetControlFlag(eControlFlag.kControlFlag_Jump, true);
+		}
+		if (input.isKeyDown(Input.KEY_A))
+		{
+			SetControlFlag(eControlFlag.kControlFlag_Left, true);
+		}
+		if (input.isKeyDown(Input.KEY_D))
+		{
+			SetControlFlag(eControlFlag.kControlFlag_Right, true);
+		}
+		if (input.isKeyDown(Input.KEY_SPACE))
+		{
+			SetControlFlag(eControlFlag.kControlFlag_Attack, true);
+		}
+		
+		super.update(gc, delta);
 	}
 }
