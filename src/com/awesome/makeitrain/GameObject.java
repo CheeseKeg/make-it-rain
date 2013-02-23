7a6e9ac803bffccd791a707997d49ec04e761ad8
btd <btd@cheesekeg.com>
2013-02-23 11:44:57 -0500

    GameObject now abstract

--- src/com/awesome/makeitrain/GameObject.java
+++ src/com/awesome/makeitrain/GameObject.java
@@ -1,11 +1,9 @@
 package com.awesome.makeitrain;
 
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
 
-public class GameObject implements Drawable {
+public abstract class GameObject implements Drawable {
+	
 	private Vector2f position;
 
 	public Vector2f getPosition() {
@@ -16,19 +14,4 @@ public class GameObject implements Drawable {
 		this.position = position;
 	}
 	
-	@Override
-	public void init(GameContainer gc) throws SlickException {
-		
-	}
-
-	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException {
-		
-	}
-
-	@Override
-	public void update(GameContainer gc, int delta) throws SlickException {
-		
-	}
-
 }
