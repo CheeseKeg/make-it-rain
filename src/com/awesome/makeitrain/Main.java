f73f9ca6655214b26bcf1387edea9e4ba530ff8a
btd <btd@cheesekeg.com>
2013-02-23 10:21:14 -0500

    Initial commit

---
+++ src/com/awesome/makeitrain/Main.java
@@ -0,0 +1,17 @@
+package com.awesome.makeitrain;
+
+public class Main {
+	MakeItRain game;
+	
+	private Main() {
+		game = new MakeItRain();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
---
+++ src/com/awesome/makeitrain/MakeItRain.java
@@ -0,0 +1,57 @@
+package com.awesome.makeitrain;
+
+import org.lwjgl.LWJGLUtil;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+
+public class MakeItRain extends BasicGame {
+	
+	private static AppGameContainer app;
+	
+	public MakeItRain() {
+		super("Make It Rain");
+		
+		// --SLICK STUFF--
+		
+		System.setProperty("org.lwjgl.librarypath", System.getProperty("user.dir") + "/lib/native/" + LWJGLUtil.getPlatformName());
+		System.setProperty("net.java.games.input.librarypath", System.getProperty("org.lwjgl.librarypath"));
+		
+		try {
+			app = new AppGameContainer(this);
+			app.setDisplayMode(800, 600, false);
+			app.setVSync(true);
+			app.setTargetFrameRate(60);
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// -- SLICK GAME METHOD OVERRIDES --
+	
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		gc.setShowFPS(true);
+	}
+	
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+	
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+		
+	}
+	
+	@Override
+	public void keyPressed(int key, char c) {
+		if (key == Input.KEY_ESCAPE)
+			app.exit();
+	}
+	
+}
