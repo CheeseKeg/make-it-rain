5a1bf9db5da3630fabc066a45265afc60d6eccf1
btd <btd@cheesekeg.com>
2013-02-25 02:01:43 -0500

    Added more getters/setters to GameObject

--- src/com/awesome/makeitrain/GameObject.java
+++ src/com/awesome/makeitrain/GameObject.java
@@ -43,7 +43,27 @@ public abstract class GameObject {
 	}
 	
 	public void setPosition(Vector2f position) {
-		boundingBox.setLocation(position.x, position.y);
+		boundingBox.setLocation(position);
+	}
+	
+	public Vector2f getCenter() {
+		return new Vector2f(
+				boundingBox.getX() + boundingBox.getWidth()/2f,
+				boundingBox.getY() + boundingBox.getHeight()/2f);
+	}
+	
+	public void setCenter(Vector2f center) {
+		boundingBox.setLocation(
+				center.x - boundingBox.getWidth()/2f,
+				center.y - boundingBox.getHeight()/2f);
+	}
+	
+	public Vector2f getSize() {
+		return new Vector2f(boundingBox.getWidth(), boundingBox.getHeight());
+	}
+	
+	public void setSize(Vector2f size) {
+		boundingBox.setSize(size.x, size.y);
 	}
 	
 	public void setVelocity(Vector2f velocity)
